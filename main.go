af000bd0423a7a87b3a60f94faa7ffead7350c53
Vitalii <vitaliy.kolodenko@gmail.com>
2021-04-24 22:49:56 -0400

    add json marshal/unmarshal test

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
@@ -26,5 +27,32 @@ func barHandler(w http.ResponseWriter, r *http.Request){
 func main() {
 	http.Handle("/foo", &fooHandler{message: "hello"})
 	http.HandleFunc("/bar", barHandler)
+
+	jsonTest()
+
 	http.ListenAndServe(":5000", nil)
+}
+
+func jsonTest(){
+	type prod struct {
+		Id int
+		Name string
+	}
+
+	rez, err := json.Marshal(&prod{Id: 1, Name: "test"})
+	if err != nil {
+		fmt.Println("Got error when encoding json", err)
+	}
+
+	fmt.Printf("JSON: %v", string(rez))
+
+
+	prodAfter := prod{}
+	err = json.Unmarshal(rez, &prodAfter)
+
+	if err != nil {
+		fmt.Println("Got error when decoding json", err)
+	}
+
+	fmt.Printf("Product after decoding: %v", prodAfter)
 }
